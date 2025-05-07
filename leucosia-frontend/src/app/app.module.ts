b91bbf867e4e96d846dd8593d2be37c5f4c164ea
hackpietro <pietrocimino3@gmail.com>
2025-05-07 18:04:37 +0200

    aggiornamenti home

--- leucosia-frontend/src/app/app.module.ts
+++ leucosia-frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
@@ -14,7 +15,7 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent }
-    ])
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- leucosia-frontend/src/app/home/home.component.ts
+++ leucosia-frontend/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +6,24 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.activateLinkOnClick();
+  }
+
+  activateLinkOnClick(): void {
+    const menuLinks = document.querySelectorAll('nav ul li a');
+
+    menuLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        menuLinks.forEach(item => item.classList.remove('active'));
+
+        link.classList.add('active');
+      });
+    });
+  }
 
 }
